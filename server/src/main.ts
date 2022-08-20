f19c576b91978b82e2c504db12ac7d5ac662407f
Belén <104005148+Nedeltoga@users.noreply.github.com>
2022-08-20 12:29:52 +0200

    #1 main.ts configurated

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function ejemplo() {
+const express = require('express');
+const spotifyWebApi = require('spotify-web-api-node');
+
+const app = express();
+
+/*
+
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
   await app.listen(3001);
 }
-ejemplo();
+bootstrap();
+*/
