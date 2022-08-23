6958a8e892415aa1f99e27d46edb83ebabbaa7dd
Edurne <edurnenun@gmail.com>
2022-08-23 15:37:44 +0200

    #2 seach function with track search

--- client/src/Components/Dashboard.tsx
+++ client/src/Components/Dashboard.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { IonContent, IonSearchbar } from '@ionic/react';
 import useAuth from './useAuth';
 import SpotifyWebApi from 'spotify-web-api-node';
+import TrackSearchResults from './TrackSearchResult';
 
 const SpotifyWebApi = new SpotifyWebApi({
   clientId:'ee2f9df1177b4f1ab271c635c6bf1219',
@@ -26,23 +27,34 @@ const Dashboard: React.FC<ContainerProps> =  ({code}) => {
   useEffect(() => {
     if (!search) return setSearchResults([])
     if (!accessToken) return
-    SpotifyWebApi.searchTracks(search).then(res () => {
-      res.body.tracks.items.map(track => {
+
+    let cancel =false
+    spotifyWebApi.searchTracks(search).then(res => {
+      if (cancel) return
+      setSearchResults(res.body.tracks.items.map(track => {
+        const smallestAlbumImage = track.album.images.reduce(smallest, image) => {
+          if (image.height < smallest.height) return image 
+          return smallest
+        }, track.album.images[0])
+
         return {
           artist: track.artists[0].name,
           title: track.name,
           uri: track.uri,
-          albumUrl: track.albumUrl.images
+          albumUrl: smallestAlbumImages.url
         }
       })
+    )
     })
-
+    return () => cancel = true
   }, [search, accessToken])
 
   return <IonContent>{code}<p>Searchbar with a placeholder</p>
   
   <IonSearchbar value={search} onIonChange={e => setSearchText(e.target.value!)} placeholder="Search songs/artist"></IonSearchbar>
-  <IonContent>Songs</IonContent>
+  <IonContent>{searchResult.map(track => (
+    <TrackSearchResult track={track} key={track.uri}></TrackSearchResult>
+  ))}</IonContent>
   </IonContent>
 
 }
