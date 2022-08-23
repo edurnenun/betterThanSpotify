b72fdfa160c5d23639302058597b29261c2c4ed9
Edurne <edurnenun@gmail.com>
2022-08-23 12:58:07 +0200

    #2 searchbar added unfunctional

--- client/src/Components/Dashboard.tsx
+++ client/src/Components/Dashboard.tsx
@@ -1,13 +1,25 @@
-import { IonContent } from '@ionic/react';
+import React, { useState } from 'react';
+import { IonContent, IonSearchbar } from '@ionic/react';
 import useAuth from './useAuth';
 
+
+
 interface ContainerProps {code?:string }
+interface SearchbarChangeEventDetail {
+  value?: string;
+}
 
 const Dashboard: React.FC<ContainerProps> =  ({code}) => {
+  const [searchText, setSearchText] = useState('');
   const accessToken = useAuth(code)
-  return <IonContent>{code}</IonContent>
+  return <IonContent>{code}<p>Searchbar with a placeholder</p>
+  
+  <IonSearchbar value={searchText} onIonChange={e => setSearchText(e.detail.value!)} placeholder="Filter Schedules"></IonSearchbar>
+  </IonContent>
 
 }
+
+
 
 export default Dashboard;
 
