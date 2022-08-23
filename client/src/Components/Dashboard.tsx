6bc10e5f454325a14d125ef7c2bb76d3e1abf60e
Belén <104005148+Nedeltoga@users.noreply.github.com>
2022-08-23 15:05:26 +0200

    #2 search function added with search tracks and useEffect

--- client/src/Components/Dashboard.tsx
+++ client/src/Components/Dashboard.tsx
@@ -1,8 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { IonContent, IonSearchbar } from '@ionic/react';
 import useAuth from './useAuth';
+import SpotifyWebApi from 'spotify-web-api-node';
 
-
+const SpotifyWebApi = new SpotifyWebApi({
+  clientId:'ee2f9df1177b4f1ab271c635c6bf1219',
+})
 
 interface ContainerProps {code?:string }
 interface SearchbarChangeEventDetail {
@@ -10,11 +13,36 @@ interface SearchbarChangeEventDetail {
 }
 
 const Dashboard: React.FC<ContainerProps> =  ({code}) => {
-  const [searchText, setSearchText] = useState('');
-  const accessToken = useAuth(code)
+  const accessToken = useAuth(code);
+  const [search, setSearch] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  
+
+  useEffect(() => {
+    if(!accessToken) return
+    SpotifyWebApi.setAccessToken(accessToken)
+  }, [accessToken])
+
+  useEffect(() => {
+    if (!search) return setSearchResults([])
+    if (!accessToken) return
+    SpotifyWebApi.searchTracks(search).then(res () => {
+      res.body.tracks.items.map(track => {
+        return {
+          artist: track.artists[0].name,
+          title: track.name,
+          uri: track.uri,
+          albumUrl: track.albumUrl.images
+        }
+      })
+    })
+
+  }, [search, accessToken])
+
   return <IonContent>{code}<p>Searchbar with a placeholder</p>
   
-  <IonSearchbar value={searchText} onIonChange={e => setSearchText(e.detail.value!)} placeholder="Filter Schedules"></IonSearchbar>
+  <IonSearchbar value={search} onIonChange={e => setSearchText(e.target.value!)} placeholder="Search songs/artist"></IonSearchbar>
+  <IonContent>Songs</IonContent>
   </IonContent>
 
 }
