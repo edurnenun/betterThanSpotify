b0622cff3bf69d77ecb776034b607817b6ec32a5
Edurne <edurnenun@gmail.com>
2022-08-23 12:36:01 +0200

    #1 edited useAuth server and ended

--- client/src/Components/useAuth.tsx
+++ client/src/Components/useAuth.tsx
@@ -2,10 +2,13 @@ import { IonContent } from "@ionic/react"
 import { useState, useEffect } from "react"
 import axios from "axios";
 
+
+
 export default function useAuth(code: any){
     const [accessToken, setAccessToken] = useState()
     const [refreshToken, setRefreshToken] = useState()
     const [expiresIn, setExpiresIn] = useState()
+
 
     useEffect(()=>{
         axios.post('http://localhost:3001/code',{
@@ -24,22 +27,23 @@ export default function useAuth(code: any){
     }, [code])
 
     useEffect(()=>{
-        axios.post('http://localhost:3001/refresh',{
+        if (!refreshToken || !expiresIn) return
+        const interval = setInterval(() => {
+            axios
+            .post('http://localhost:3001/refresh',{
             refreshToken,
-        })
-        .then(res => {
-            /*
+            })
+            .then(res => {
             setAccessToken(res.data.accessToken)
-            setRefreshToken(res.data.refreshToken)
-            setExpiresIn(res.data.expiresIn)
-            window.history.pushState({},null!, '/')
-            */
-        })
-        .catch(()=>{
+            setExpiresIn(res.data.expiresIn)        
+            })
+            .catch(()=>{
             const win: Window = window;
             win.location = "/"
-        })
+            })
+        }, (expiresIn - 60) * 1000)
 
+        return () => clearInterval(interval)
     }, [refreshToken, expiresIn])
 
     return accessToken
