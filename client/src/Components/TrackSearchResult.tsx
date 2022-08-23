ef00e83a404ff53b8cad3699c11ab8aa52a9d603
Edurne <edurnenun@gmail.com>
2022-08-23 15:59:33 +0200

    #2 TrackSearchResults added with images, title and artist tracks

--- client/src/Components/TrackSearchResult.tsx
+++ client/src/Components/TrackSearchResult.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import { IonContent, IonImg, IonItemDivider } from '@ionic/react';
 
 export default function TrackSearchResults() {
-    return
+    return (
+        <IonContent>
+            <IonImg src={track.albumUrl} style={{ height: "64px", width: "64px"}} />
+           <IonItemDivider>{track.title} </IonItemDivider> 
+           <IonItemDivider>{track.artist} </IonItemDivider> 
+        </IonContent>
+    )
 }
